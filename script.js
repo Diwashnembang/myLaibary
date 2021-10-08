944c8022eed27860677ba9e7f2b345ca8fb5719c
Diwash Nembang <nembangd0@gmail.com>
2021-10-09 00:08:22 +0545

    added gui

--- script.js
+++ script.js
@@ -2,151 +2,175 @@
  -diwash nembang 
  the odin project*/
 
-function book(title,aurthor,totalPage,status){
-    this.title=title;
-    this.aurthor=aurthor;
-    this.totalPage=totalPage;
-    this.status=status;
-    this.info=()=>{
-        return `${title} by ${aurthor},${totalPage} pages,${status}`
-    }
-}
-  
-// book.prototype.remove(){
-//   myLaibary.splice
-// }
-
-function makecards(){
-
-
-for (let  book= 0;  book< myLaibary.length; book++) {
-  const copiedDom=cardDom.cloneNode(true);
-  copiedDom.classList.remove("hidden");
-   mainDom.append(copiedDom);
-   copiedDom.setAttribute("data-cardNo",`${book}`);
-   cardContentLogic(myLaibary[book],copiedDom);
-   cardNumber++;
-  
+function book(title, aurthor, totalPage, status, read) {
+  this.title = title;
+  this.aurthor = aurthor;
+  this.totalPage = totalPage;
+  this.status = status;
+  this.read = read;
+  this.info = () => {
+    return `${title} by ${aurthor},${totalPage} pages,${status}`;
+  };
 }
 
-   const cards=Array.from(document.querySelectorAll(".cardDelet"));
-   cards.forEach(card=>{
-      card.addEventListener("click",()=>{
-        removeCard(card)
-      })
+function changeRead(book, add) {
+  if (add) {
+    if (book.status >= book.totalPage) return;
+    book.status = +book.status + 1;
+  } else {
+    if (book.status <= 0) return;
+    book.status = +book.status - 1;
+  }
+  clearScreen();
+  makecards();
+}
+function makecards() {
+  for (let book = 0; book < myLaibary.length; book++) {
+    const copiedDom = cardDom.cloneNode(true);
+    copiedDom.classList.remove("hidden");
+    mainDom.append(copiedDom);
+    copiedDom.setAttribute("data-cardNo", `${book}`);
+    cardContentLogic(myLaibary[book], copiedDom);
+    cardNumber++;
+  }
+
+  const cards = Array.from(document.querySelectorAll(".cardDelet"));
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      removeCard(card);
+    });
 
     //  console.log(card.parentNode.parentNode.getAttribute("data-cardNo"))
-   })
-   
-
-  
+  });
 }
 
-function removeCard(card){
-  myLaibary.splice(+card.parentNode.parentNode.getAttribute("data-cardNo"),1);
+function removeCard(card) {
+  myLaibary.splice(+card.parentNode.parentNode.getAttribute("data-cardNo"), 1);
   clearScreen();
   makecards();
   console.log(myLaibary);
 }
 
-function cardContentLogic(book,copiedDom){
-  copiedDomChildrens=Array.from(copiedDom.children);
-  copiedDomChildrens.forEach(child=>{
-       if(child.getAttribute("class")==="top"){
+function cardContentLogic(book, copiedDom) {
+  copiedDomChildrens = Array.from(copiedDom.children);
+  copiedDomChildrens.forEach((child) => {
+    if (child.getAttribute("class") === "cardAurthor") {
+      child.textContent = `By-${book.aurthor.toUpperCase()}`;
+    }
+    if (child.getAttribute("class") === "top") {
+      child.firstElementChild.textContent = book.title.toUpperCase(); //this is #titile
+      //  console.log()
+    }
+    if (child.getAttribute("class") === "cardTotalPage") {
+      child.textContent = `Pages: ${book.totalPage}`;
+    }
 
-     child.firstChild.textContent=book.title;
-    //  console.log()
-   }
-   if(child.getAttribute("class")==="cardAurthor"){
+    if (child.getAttribute("class") === "cardTotalRead") {
+      child.textContent = `Completed: ${book.status}`;
+    }
+    if (child.getAttribute("class") === "cardEditSection") {
+      let numberInput =
+        child.firstElementChild.nextElementSibling.nextElementSibling;
+      let add = child.firstElementChild.nextElementSibling;
+      let minus =
+        child.firstElementChild.nextElementSibling.nextElementSibling
+          .nextElementSibling;
 
-     child.textContent=book.aurthor;
-   
-  }
-  if(child.getAttribute("class")==="cardTotalPage"){
+      numberInput.value = book.status;
 
-    child.textContent=book.totalPage;
-   
-  }
+      numberInput.addEventListener("keydown", (e) => {
+        if (e.key !== "Enter") return;
+        if (numberInput.value <= 0) numberInput.value = 0;
+        if (numberInput.value >= book.totalPage)
+          numberInput.value = book.totalPage;
+        book.status = numberInput.value;
+        clearScreen();
+        makecards();
+      });
 
+      add.addEventListener("click", () => {
+        changeRead(book, true);
+      });
+      minus.addEventListener("click", () => {
+        changeRead(book, false);
+      });
+    }
   });
+}
+function isValid(e) {
+  if (totalPageDom.value === "" || completedDom.value === "") return;
 
-   
- }
+  if (totalPageDom.value < completedDom.value) {
+    e.preventDefault();
 
+    return alert(errorMessage.moreCompletedPage);
+  }
 
-function addBook(){
-  if(titleDom.value===''||aurthorDom.value===''||totalPage.value===''){return console.log("returned")}
-    let newBook=new book(titleDom.value,aurthorDom.value,totalPage.value,completedDom.value);
-    myLaibary.push(newBook);
-   
-  console.table(myLaibary);
+  if (completedDom.value < 0) {
+    e.preventDefault();
 
-  
+    return alert(errorMessage.negativeCompletedPage);
+  }
+}
+function addBook(e) {
+  isValid(e);
+
+  let newBook = new book(
+    titleDom.value,
+    aurthorDom.value,
+    totalPageDom.value,
+    completedDom.value
+  );
+  myLaibary.push(newBook);
+
+  clearAddBookFrom();
+  clearScreen();
+  makecards();
 }
 
-
-function clearScreen(){
-  while(mainDom.firstChild){
+function clearScreen() {
+  while (mainDom.firstChild) {
     mainDom.removeChild(main.firstChild);
   }
 }
 
-
-function clearAddBookFrom(){
-  aurthorDom.value='';
-  titleDom.value='';
-  totalPageDom.value='';
-  completedDom.value='';
+function clearAddBookFrom() {
+  aurthorDom.value = "";
+  titleDom.value = "";
+  totalPageDom.value = "";
+  completedDom.value = "";
 }
 
+let myLaibary = [];
+let cardNumber = 0;
+let errorMessage = {
+  moreCompletedPage: "Completed Page Can't Be More Than Total Page",
+  negativeCompletedPage: "Completed Page can't be Negative",
+};
 
-
-
-let myLaibary=[];
-let  cardNumber=0;
-
-
-
-
-
-
-
-//general doms 
-const mainDom=document.querySelector("#main");
-const cardDom=document.querySelector(".card");
-const addBookButtonDom=document.querySelector("#info");
-const addFormDom=document.querySelector("#addForm")
-
+//general doms
+const mainDom = document.querySelector("#main");
+const cardDom = document.querySelector(".card");
+const addBookButtonDom = document.querySelector("#info");
+const addFormDom = document.querySelector("#addForm");
 
 // doms for adding books
-const aurthorDom=document.querySelector("#aurthor");
-const titleDom=document.querySelector("#title");
-const totalPageDom=document.querySelector("#totalPage");
-const completedDom=document.querySelector("#completed");
-const submmitButtonDom=document.querySelector("#button");
+const aurthorDom = document.querySelector("#aurthor");
+const titleDom = document.querySelector("#title");
+const totalPageDom = document.querySelector("#totalPage");
+const completedDom = document.querySelector("#completed");
+const submmitButtonDom = document.querySelector("#button");
 
 // card doms
-const cardTitleDom=document.querySelector(".cardTitle");
-const cardAurthorDom=document.querySelector(".cardAurthor");
-const cardTotalPageDom=document.querySelector(".cardTotalPage");
-console.log(cardTitleDom);
+const addDom = document.querySelector(".add");
+const minusDom = document.querySelector(".minus");
 
+// read slider dom
 
-addBookButtonDom.addEventListener("click",()=>{
-  addFormDom.classList.toggle("hidden")
-})
+addBookButtonDom.addEventListener("click", () => {
+  addFormDom.classList.toggle("hidden");
+});
 
-
-submmitButtonDom.addEventListener("click",()=>{
-  addBook();
-  clearScreen()
-  makecards();
-  clearAddBookFrom();
-})
-
-
-removeCardDom.addEventListener("click",(e)=>{
-  console.log(e)
-  console.log("clicked")
-})
-
+submmitButtonDom.addEventListener("click", (e) => {
+  addBook(e);
+});
